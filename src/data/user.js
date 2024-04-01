1047ff17d54175d7d852a77007f417d0a4875cc5
Chamindu Nevandith <152043450+ChaminduNevandith@users.noreply.github.com>
2024-04-01 20:07:09 +0530

    src  files commmit

---
+++ src/data/user.js
@@ -0,0 +1,77 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Chamindu",
+		name: "S.C.Nevandith",
+		email: "cnewandith123@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/ChaminduNevandith",
+		linkedin: "https://www.linkedin.com/in/chamindu-nevandith-1691b9278/",
+		instagram: "https://www.instagram.com/chami__du/",
+
+	},
+
+	homepage: {
+		title: "Full-stack web application developer, and UI/UX designer.",
+		description:
+			"I am a frontend developer specializing in React.js, with a focus on creating engaging user interfaces and exceptional user experiences. I have a strong foundation in UI/UX principles and enjoy translating design mockups into functional components. With a meticulous approach to coding, I strive to deliver clean, efficient, and maintainable code that adheres to industry best practices and backend developer with expertise in Node.js.",
+	},
+
+	about: {
+		title: "I’m Chamindu Nevandith.I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on many different projects over the years, and I'm really proud of what I've achieved. A lot of these projects are open for anyone to look at and even help make better. If you're curious about any of the projects I've worked on, you're welcome to take a look at the code and maybe even suggest ways to make them even better. Working together with others is a fantastic way to learn new things and get better at what we do, and I'm always excited to hear new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Book recommendation system",
+			description:
+				"University software development group project using react , node.js ,Lang chain and openAI. chatbot base book recommendation webapplication.",
+			linkText: "View Project",
+			link: "https://github.com/HarborReads/HarborReads-Dashboard",
+		},
+
+		{
+			title: "Music Website",
+			description:
+				"University group project using web technologies. Details about the evolution of music throughout history, including rock, pop, hip hop, rap, and some important details about musical groups.(HTML/CSS/JAVASCRIPT)",
+			linkText: "View Project",
+			link: "https://github.com/ChaminduNevandith/Music_website",
+		},
+
+		{
+			title: "Shopping Management System",
+			description:
+				"Application that use for manage shopping managment system with java swing with GUI interface created using Java,Java Swing",
+			linkText: "View Project",
+			link: "https://github.com/ChaminduNevandith/Shopping_Management_System",
+		},
+
+		{
+			title: "School Student Register",
+			description:
+				"Application created to get results for models and output the eligibility of the student to proceed to next academic year (Python). ",
+			linkText: "View Project",
+			link: "https://github.com/ChaminduNevandith/School_Student_Register",
+		},
+
+		{
+			title: "UI/UX Designs ",
+			description:
+				"There are UI/UX designs that i did with my team and these UI/UX designs used to designs for the university project and some of them designs for competitions",
+			linkText: "View Project",
+			link: "https://github.com/ChaminduNevandith/UI-UX_Designs",
+		},
+	],
+};
+
+export default INFO;
